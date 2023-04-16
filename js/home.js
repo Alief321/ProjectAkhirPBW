1368cbb4c202dc740a4b934c1fd5f7b755d059dd
Alief Zulvan <depanb187@gmail.com>
2023-04-16 13:41:09 +0700

    added animation

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
+const hiro = document.querySelector('#hiro_pic');
+const stop = document.getElementById('stop');
+hiro.addEventListener('click', function () {
+  hiro.classList.add('hiro-pic-spin');
+  stop.style.display = 'flex';
+});
 
+stop.addEventListener('click', function () {
+  hiro.classList.remove('hiro-pic-spin');
+  stop.style.display = 'none';
+});
