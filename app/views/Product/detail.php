172354dcb64f56a62a3d7f66ff5fd7ec2dbbf69e
Alief Zulvan <depanb187@gmail.com>
2023-04-23 14:51:11 +0700

    detai; page done

--- app/views/Product/detail.php
+++ app/views/Product/detail.php
@@ -1,45 +1,55 @@
     <!-- Isi -->
     <section id="product-detail">
-    <div class="prod-pic">
-          <img src="img/whiskas.png" alt="whiskas" width="60%" />
+        <div class="prod-pic">
+          <div style="display: flex; width: 100%; justify-content: right; height: auto;">
+            <i data-feather="shopping-cart" id="shoping-cart"></i>
+          </div>
+          <div class="img-pic">
+            <img src="img/whiskas.png" alt="whiskas" />
+          </div>
         </div>
         <div class="prod-text">
-            <i data-feather="shopping-cart"></i>
-            <p>RP29.502</p>
+          <p class="prod-judul">Whiskas 4Pcs Pouch Sachet Adult Tuna And White Fish Flavour</p>
+          <p class="prod-price">RP29.502</p>
+          <div class="stok">
             <p>Stok Barang</p>
             <p>100</p>
-            <p class="prod-judul">Whiskas 4Pcs Pouch Sachet Adult Tuna And White Fish Flavour</p>
-            <p style="font-size: 0.75rem; margin-top: 0; text-align: justify">Kalau untuk kucing kesayangan, pastinya kamu harus memberikan makanan bernutrisi terbaik! Whiskas Pouch Adult 1+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur, exercitationem.</p>
-            <p>Link pembelian</p>
+          </div>
+          <hr>
+          <p>Deskripsi Product</p>
+          <p class="desk-detail">Kalau untuk kucing kesayangan, pastinya kamu harus memberikan makanan bernutrisi terbaik! Whiskas Pouch Adult 1+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur, exercitationem.</p>
+          <p>Link pembelian</p>
+          <div class="link-olshop">
             <a href="" class="link">
-            <div class="isi-content" style="background: #ff8678">
-              <img src="img/shopee.png" alt="shopee" width="25%" height="90%" />
-              <p>Shopee</p>
-            </div>
-          </a>
-          <a href="" class="link">
-            <div class="isi-content" style="background: #74c26f">
-              <img src="img/tokped.png" alt="shopee" width="25%" height="90%" />
-              <p>Tokopedia</p>
-            </div>
-          </a>
-          <a href="" class="link">
-            <div class="isi-content" style="background: #83c5e3">
-              <img src="img/blibli.png" alt="shopee" width="25%" height="90%" />
-              <p>Blibli</p>
-            </div>
-          </a>
-          <a href="" class="link">
-            <div class="isi-content laz">
-              <img src="img/lazada.png" alt="lazada" width="25%" height="90%" />
-              <p>Lazada</p>
-            </div>
-          </a>
-          <a href="" class="link">
-            <div class="isi-content" style="background: #f377ad">
-              <img src="img/bukalapak.png" alt="bukalapak" width="25%" height="90%" />
-              <p>Bukalapak</p>
-            </div>
-          </a>
+              <div class="isi-content" style="background: #ff8678">
+                <img src="img/shopee.png" alt="shopee" width="25%" height="90%" />
+                <p>Shopee</p>
+              </div>
+            </a>
+            <a href="" class="link">
+              <div class="isi-content" style="background: #74c26f">
+                <img src="img/tokped.png" alt="shopee" width="25%" height="90%" />
+                <p>Tokopedia</p>
+              </div>
+            </a>
+            <a href="" class="link">
+              <div class="isi-content" style="background: #83c5e3">
+                <img src="img/blibli.png" alt="shopee" width="25%" height="90%" />
+                <p>Blibli</p>
+              </div>
+            </a>
+            <a href="" class="link">
+              <div class="isi-content laz">
+                <img src="img/lazada.png" alt="lazada" width="25%" height="90%" />
+                <p>Lazada</p>
+              </div>
+            </a>
+            <a href="" class="link">
+              <div class="isi-content" style="background: #f377ad">
+                <img src="img/bukalapak.png" alt="bukalapak" width="25%" height="90%" />
+                <p>Bukalapak</p>
+              </div>
+            </a>
+          </div>
     </section>
     <!-- Akhir Isi -->
